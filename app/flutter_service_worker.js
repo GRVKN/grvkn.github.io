3700f8d31307951843fa12bd8707ba0263e815ee
Andre Hallau <grvkn2025@gmail.com>
2026-08-21 22:57:56 +0200

    Publish GRVKN landing and web app

--- app/flutter_service_worker.js
+++ app/flutter_service_worker.js
@@ -39,13 +39,13 @@ const RESOURCES = {"assets/AssetManifest.bin": "021a0fe5b91372c5f801a588ad67b32b
 "canvaskit/skwasm_heavy.js.symbols": "0755b4fb399918388d71b59ad390b055",
 "canvaskit/skwasm_heavy.wasm": "b0be7910760d205ea4e011458df6ee01",
 "flutter.js": "24bc71911b75b5f8135c949e27a2984e",
-"flutter_bootstrap.js": "3fe7f5209aaa9424b0effff3a073dadd",
+"flutter_bootstrap.js": "785337db3d626ecca747ddaa2b3a0d4c",
 "icons/grvkn.svg": "2a7103b579f500a61038c607b03feac2",
 "index.html": "9ec929819f715dcdd74c254c7dae8004",
 "/": "9ec929819f715dcdd74c254c7dae8004",
-"main.dart.js": "30e4d3b59d1f7342778ec688e300d704",
+"main.dart.js": "fc1ac375893ef5280a0340fdd9fb0938",
 "manifest.json": "cd37f0070268baa99c559a3b39db6ba4",
-"version.json": "14b34b53a6ab6b14900694574b400764"};
+"version.json": "7a859d35ac0c93a84a951df05d640807"};
 // The application shell files that are downloaded before a service worker can
 // start.
 const CORE = ["main.dart.js",
