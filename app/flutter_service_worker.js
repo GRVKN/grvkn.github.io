8b53a6303eb3bf8f098e799ed06d8a3f0522d600
Andre Hallau <grvkn2025@gmail.com>
2026-08-24 17:49:45 +0200

    Publish GRVKN landing and web app

--- app/flutter_service_worker.js
+++ app/flutter_service_worker.js
@@ -22,7 +22,7 @@ const RESOURCES = {"assets/AssetManifest.bin": "021a0fe5b91372c5f801a588ad67b32b
 "assets/assets/demo_videos/regional_burger.mp4": "645ed71c1fe5134586c68bb66cbc4ce6",
 "assets/assets/demo_videos/regional_farm_shop.mp4": "7de32cfcb4989098abac0cc0ee372e8b",
 "assets/FontManifest.json": "7b2a36307916a9721811788013e65289",
-"assets/fonts/MaterialIcons-Regular.otf": "59ff0aefcb7b97f9efbd6b5fb64d6fe9",
+"assets/fonts/MaterialIcons-Regular.otf": "e022d922f1511976cacc23d52d5d6b25",
 "assets/NOTICES": "6ef7e22df244c5442dec29bc73e61e3d",
 "assets/shaders/ink_sparkle.frag": "ecc85a2e95f5e9f53123dcaf8cb9b6ce",
 "assets/shaders/stretch_effect.frag": "40d68efbbf360632f614c731219e95f0",
@@ -39,13 +39,13 @@ const RESOURCES = {"assets/AssetManifest.bin": "021a0fe5b91372c5f801a588ad67b32b
 "canvaskit/skwasm_heavy.js.symbols": "0755b4fb399918388d71b59ad390b055",
 "canvaskit/skwasm_heavy.wasm": "b0be7910760d205ea4e011458df6ee01",
 "flutter.js": "24bc71911b75b5f8135c949e27a2984e",
-"flutter_bootstrap.js": "8809302c193f847a5634c1121a8f3b47",
+"flutter_bootstrap.js": "478bea4d94e4e1d9c32c6d55fc6b70fc",
 "icons/grvkn.svg": "2a7103b579f500a61038c607b03feac2",
 "index.html": "9ec929819f715dcdd74c254c7dae8004",
 "/": "9ec929819f715dcdd74c254c7dae8004",
-"main.dart.js": "ba94008bec12497303be1811f273022a",
+"main.dart.js": "8035ef305cbe97718429167f928ee4ae",
 "manifest.json": "cd37f0070268baa99c559a3b39db6ba4",
-"version.json": "f070f757773ec82485764d8a1e39a524"};
+"version.json": "9977ea0d86b320410c12536a82a246fa"};
 // The application shell files that are downloaded before a service worker can
 // start.
 const CORE = ["main.dart.js",
